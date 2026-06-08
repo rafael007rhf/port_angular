493a6158d63b619c9154e038a08e34122a77dadb
rafael007rhf <rafa.henrique.freire@gmail.com>
2026-06-08 20:25:04 +0000

    Aula16: Cria componentes, navegacao com Material e rotas do Portfolio

--- port_angular/src/app/app.ts
+++ port_angular/src/app/app.ts
@@ -1,12 +1,11 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-
+import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+selector: 'app-root',
+imports: [RouterOutlet, RouterLink, RouterLinkActive, MatToolbarModule, MatButtonModule],
+templateUrl: './app.html',
+styleUrl: './app.css'
 })
-export class App {
-  protected readonly title = signal('Meu Portfólio');
-}
+export class App {}
---
+++ port_angular/src/app/projetos/projetos.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { MatCardModule} from '@angular/material/card'
+
+@Component({
+  selector: 'app-projetos',
+  imports: [MatCardModule],
+  templateUrl: './projetos.html',
+  styleUrl: './projetos.css',
+})
+export class Projetos {}
